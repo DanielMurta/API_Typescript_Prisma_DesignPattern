7ea0c4206e4a695c1df1167e31081a4044a9e35c
Daniel Silveira <murtdaniel@gmail.com>
2024-01-31 15:23:34 -0300

    feat: Post controllers

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,106 @@
+import { Request, Response } from "express";
+import { prisma } from "../database";
+
+export default {
+  async createPost(request: Request, response: Response){
+    try {
+      const { title, content, userId } = request.body;
+      const post = await prisma.post.create({ data: {
+        title: title,
+        content: content,
+        userId: userId
+      }});
+
+      return response.json({
+        erro: false,
+        message: 'Success: Post created',
+        post
+      });
+
+    } catch (error) {
+      return response.json({ massage: error.massage })
+    }
+  },
+
+  async ListPost(request: Request, response: Response){
+    try {
+      const { id } = request.params;  
+      const posts = await prisma.post.findUnique({ where: { id: Number(id) }});
+      
+      if(!posts){
+        return response.json({ 
+          erro: true,
+          message: 'Erro: Post not found' 
+        });
+      }
+
+      return response.json({
+        erro: false,
+        posts
+      });
+
+    } catch (error) {
+      return response.json({ massage: error.massage })
+    }
+  },
+
+  async UpdatePost(request: Request, response: Response){
+    try {
+      const { id, title, content } = request.body;
+
+      const postExist = await prisma.post.findUnique({ where: { id: Number(id) }});
+      
+      if(!postExist){
+        return response.json({ 
+          erro: true,
+          message: 'Erro: Post not found' 
+        });
+      }
+
+      const post = await prisma.post.update( { 
+        where: {
+            id: Number(request.body.id)
+        },
+        data: {
+            title,
+            content
+        }
+      });
+
+      return response.json({
+        erro: false,
+        message: 'Success: Post updated',
+        post
+      });
+
+    } catch (error) {
+      return response.json({ massage: error.massage })
+    }
+  },
+
+  async DeletePost(request: Request, response: Response){
+    try {
+      const { id } = request.params;
+
+      const postExist = await prisma.post.findUnique({ where: { id: Number(id) }});
+      
+      if(!postExist){
+        return response.json({ 
+          erro: true,
+          message: 'Erro: Post not found' 
+        });
+      }
+
+      const post = await prisma.post.delete( { where: { id: Number(id) }});
+
+      return response.json({
+        erro: false,
+        message: 'Success: Post deleted',
+        post
+      });
+
+    } catch (error) {
+      return response.json({ massage: error.massage })
+    }
+  },
+};
